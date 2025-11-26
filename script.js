3e22f0e32dad94ff7a89c1b61272a3c71b191163
Ethan Sarante <ethansarante@gmail.com>
2025-11-26 12:10:17 -0500

    Add form submission handling in script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("uploadForm");
+const image = document.getElementById("imageFile");
+const messageArea = document.getElementById("messageArea");
+
+let userMessage = `<div class="msg user"></div>`
+let assistantMessage = `<div class="msg assistant"></div>`
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault(); // stops form from clearing page
+
+    // Need to fetch Python data
+
+})
