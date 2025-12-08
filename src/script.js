50cbd1bd8fd0ba1583a3faadae623af355fe9401
Ethan Sarante <ethansarante@gmail.com>
2025-12-07 22:14:15 -0500

    Refactor form submission and add error handling

--- src/script.js
+++ src/script.js
@@ -1,13 +1,49 @@
-const form = document.getElementById("uploadForm");
-const image = document.getElementById("imageFile");
-const messageArea = document.getElementById("messageArea");
 
-let userMessage = `<div class="msg user"></div>`
-let assistantMessage = `<div class="msg assistant"></div>`
+// Use the prject by:
+//      1. Enable the Python backend by running "py detection.py" in your powershell terminal
+//      2. Open index.html file in a new browser (preferrably Chrome)
+//      3. An error will happen if the server isn't running or if there is an issue with the API which I doubt. btw ctrl+c to close the server
 
-form.addEventListener('submit', async function(e){
-    e.preventDefault(); // stops form from clearing page
+document.getElementById("uploadForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-    // Need to fetch Python data
+    const file = document.getElementById("imageFile").files[0];
+    if (!file) return;
 
-})
+    addMessage("user", "Locating your image...");
+
+    let formData = new FormData();
+    formData.append("image", file);
+
+    try{
+        const res = await fetch("http://127.0.0.1:5000/analyze", {
+            method: "POST",
+            body: formData
+        });
+
+        let data = await res.json();
+
+        if (data.error) {
+            addMessage("assistant", "Server error: " + data.error);
+            return;
+        }
+
+        addMessage("assistant", data.result);
+        addMessage("assistant", `Estimated coords: ${data.latitude}, ${data.longitude}`);
+
+    }
+    catch (err){
+        addMessage("assistant", "Something went wrong.");
+        console.error(err, "Request/Server Error. Ensure the server is running before testing.");
+    }
+});
+
+function addMessage(role, text){
+    const msgArea = document.getElementById("messageArea");
+    const div = document.createElement("div");
+
+    div.className = `msg ${role}`;
+    div.textContent = text;
+    
+    msgArea.appendChild(div);
+}
